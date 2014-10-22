dd0b26ebd6e24fdf472a4ed5512c3ee38b972a96
Cesar A. Bernardini <bernardc@chouffe.loria.fr>
2014-10-22 13:32:32 +0200

    corrections on absolute path addresses

--- build_regulartraffic.sh
+++ build_regulartraffic.sh
@@ -3,7 +3,7 @@
 #first argument configuration file
 # second argument trace file to generate
 
-python traffic.py $1 | sort -k 1 -t ' ' -n > /data/traza;
-python complete_client_server_traces.py $1 /data/traza > /data/traza_header;
-cat /data/traza_header /data/traza > /data/traza2;
-python mobility.py $1 /data/traza2 > $2;
+python traffic.py $1 | sort -k 1 -t ' ' -n > /tmp/traza;
+python complete_client_server_traces.py $1 /tmp/traza > /tmp/traza_header;
+cat /data/traza_header /tmp/traza > /tmp/traza2;
+python mobility.py $1 /tmp/traza2 > $2;
