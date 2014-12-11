7aed3ebb95bd2452ad4341737875447245c9aefe
Cesar Adrian Bernardini <bernardi3@fst-ats-319-l20.fst.site.univ-lorraine.fr>
2014-12-11 16:17:36 +0100

    test in a different computer of sonetor capabilities

--- build_regulartraffic.sh
+++ build_regulartraffic.sh
@@ -5,5 +5,5 @@
 
 python traffic.py $1 | sort -k 1 -t ' ' -n > /tmp/traza;
 python complete_client_server_traces.py $1 /tmp/traza > /tmp/traza_header;
-cat /data/traza_header /tmp/traza > /tmp/traza2;
+cat /tmp/traza_header /tmp/traza > /tmp/traza2;
 python mobility.py $1 /tmp/traza2 > $2;
